0863448ab9b3c1357e93071943a679edec4fd3db
Symbiote231 <rwarjrr@gmail.com>
2025-01-26 23:57:56 -0600

    Initiated CodeIgniter 4 AppStarter project using autoRoutesImproved feature

---
+++ app/Controllers/Home.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Home extends BaseController
+{
+    public function getIndex(): string  // getIndex instead of index to leverage autoRoutesImproved feature
+    {
+        return view('welcome_message');
+    }
+}
